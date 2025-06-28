e92bad8184b92c87d42b8d2e4057f504ae20d436
Lucas Guidi <lucasrguidi@gmail.com>
2025-06-28 18:19:21 -0300

    feat: :sparkles: changes in user summary

--- src/components/user-summary.tsx
+++ src/components/user-summary.tsx
@@ -1,4 +1,4 @@
-import { Volleyball } from 'lucide-react';
+import { AlignLeft, Trophy, Volleyball } from 'lucide-react';
 
 export default function UserSummary() {
   return (
@@ -12,17 +12,17 @@ export default function UserSummary() {
           </div>
         </div>
         <div className="flex gap-2 items-center">
-          <Volleyball size={52} className="text-white" />
+          <AlignLeft size={52} className="text-white" />
           <div className="flex flex-col justify-between">
-            <span className="text-white text-md font-bold">Tipo de Quadra</span>
-            <span className="text-white text-md font-light">Society</span>
+            <span className="text-white text-md font-bold">Nível</span>
+            <span className="text-white text-md font-light">Semi-Profissional</span>
           </div>
         </div>
         <div className="flex gap-2 items-center">
-          <Volleyball size={52} className="text-white" />
+          <Trophy size={52} className="text-white" />
           <div className="flex flex-col justify-between">
-            <span className="text-white text-md font-bold">Tipo de Quadra</span>
-            <span className="text-white text-md font-light">Society</span>
+            <span className="text-white text-md font-bold">Vitórias</span>
+            <span className="text-white text-md font-light">345</span>
           </div>
         </div>
       </div>
